bfbb503556de612b7104ab1d2442f0710070adab
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2022-08-12 19:34:31 +0530

    Cache clear

--- client/public/sw.js
+++ client/public/sw.js
@@ -2,10 +2,10 @@ self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
+                "./",
                 "./images/logo192.png",
                 "./js/bundle.js",
                 "./js/index.js",
-                "./",
             ]);
         })
     )
