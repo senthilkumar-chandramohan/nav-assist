11145d07db9c795f53b4daf27bc2e56d9caf0521
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2022-08-12 21:30:13 +0530

    styling fix

--- client/public/sw.js
+++ client/public/sw.js
@@ -3,9 +3,9 @@ self.addEventListener("install", e => {
         caches.open("static").then(cache => {
             return cache.addAll([
                 "./",
-                "./images/logo192.png",
                 "./js/bundle.js",
                 "./js/index.js",
+                "./images/logo192.png",
             ]);
         })
     )
