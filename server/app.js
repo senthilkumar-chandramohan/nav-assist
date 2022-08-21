bec009ca2bb7357d1e3d6cd9129b424c89cf53a2
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2022-08-22 03:25:12 +0530

    Significant changes to workflow

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const path = require("path");
 
-const { createAndTrainModel, predict, loadModel } = require("./modules/model");
-const { extractPixelDataFromImages, writePixelDataToCSV, splitCSV } = require("./modules/utils");
+const { createModel, loadModel, loadDataAndTrainModel, predict } = require("./modules/model");
+const { extractPixelDataFromImages, writePixelDataToCSV, splitDataSetCSV } = require("./modules/utils");
 
 const app = express();
 
 app.set("port", (process.env.PORT || 5000));
 app.use("/", express.static(path.join(__dirname, "../client/public")));
 
-app.get("/extract-pixel-data-from-images", (req, res) => {
-    extractPixelDataFromImages();
+app.get("/extract-pixel-data-from-images/dataset/:dataSetName", (req, res) => {
+    extractPixelDataFromImages(req.params.dataSetName);
     res.send("Extraction started!");
 });
 
-app.get("/export-to-csv", (req, res) => {
-    writePixelDataToCSV();
+app.get("/export-to-csv/dataset/:dataSetName", (req, res) => {
+    writePixelDataToCSV(req.params.dataSetName);
     res.send("Export started!");
 });
 
-app.get("/train-model", async (req, res) => {
-    createAndTrainModel();
+app.get("/create-model", async (req, res) => {
+    createModel();
     res.send("Model Training Initiated...");
 });
 
@@ -29,16 +29,29 @@ app.get("/load-model", async (req, res) => {
     res.send(result === true ? "Model loaded successfully!" : result);
 });
 
-app.get("/split-csv", (req, res) => {
-    splitCSV();
+app.get("/train-model/dataset/:dataSetName", async (req, res) => {
+    loadDataAndTrainModel(req.params.dataSetName);
+    res.send("Model Training Initiated...");
+});
+
+app.get("/split-csv/dataset/:dataSetName/:min/:max", (req, res) => {
+    const {
+        params: {
+            dataSetName,
+            min,
+            max,
+        }
+    } = req;
+
+    splitDataSetCSV(dataSetName, min, max);
     res.send("CSV Split started");
 });
 
 app.get("/predict", async (req, res) => {
-    const predictedNumber = await predict(path.join(__dirname, "./predict.png"));
+    const predictedValue = await predict(path.join(__dirname, "./predict.png"));
 
-    if (predictedNumber) {
-        res.status(200).json(predictedNumber);
+    if (predictedValue) {
+        res.status(200).json(predictedValue);
     } else {
         res.status(404).json({error: "Model not ready"});
     }
--- server/modules/model.js
+++ server/modules/model.js
@@ -1,8 +1,8 @@
 const tf = require("@tensorflow/tfjs-node");
 const path = require("path");
-const { toPixelData } = require("./utils");
+const { toPixelData, getDataSetFilenameFragment } = require("./utils");
 
-let model, modelReady = false;
+let model, modelCreated, modelReadyForPrediction;
 
 const labels = [
     '0',
@@ -41,32 +41,32 @@ const labels = [
     'X',
     'Y',
     'Z',
-    // 'a',
-    // 'b',
-    // 'c',
-    // 'd',
-    // 'e',
-    // 'f',
-    // 'g',
-    // 'h',
-    // 'i',
-    // 'j',
-    // 'k',
-    // 'l',
-    // 'm',
-    // 'n',
-    // 'o',
-    // 'p',
-    // 'q',
-    // 'r',
-    // 's',
-    // 't',
-    // 'u',
-    // 'v',
-    // 'w',
-    // 'x',
-    // 'y',
-    // 'z',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
     '_',
     ',',
     '.',
@@ -81,18 +81,9 @@ const imageChannels = 1;
 const batchSize = 1000;
 const epochsValue = 50;
 
-const loadModel = async () => {
-    try {
-        model = await tf.loadLayersModel(`file://${path.join(__dirname, "../../client/public/model/model.json")}`);
-        modelReady = true;
-        return true;
-    } catch(exp) {
-        return exp;
-    }
-}
-
 const createModel = () => {
-    const model = tf.sequential();
+    modelReadyForPrediction = false;
+    model = tf.sequential();
 
     // Add model layers
     model.add(tf.layers.conv2d({
@@ -133,49 +124,26 @@ const createModel = () => {
         metrics: ["accuracy"]
     });
 
-    return model;
+    modelCreated = true;
 };
 
-const trainModel = async (model, trainingData, epochs = epochsValue) => {
-    const options = {
-        epochs,
-        batchSize,
-        verbose: 0,
-        callbacks: {
-            onEpochBegin: (epoch) => {
-                console.log(`Epoch ${epoch + 1} of ${epochs}`);
-            },
-            onEpochEnd: (epoch, logs) => {
-                console.log(`Training set loss: ${logs.loss.toFixed(4)}`);
-                console.log(`Training set accuracy: ${logs.acc.toFixed(4)}`);
-            }
-        }
-    };
-
-    return await model.fitDataset(trainingData, options);
-};
-
-const evaluateModel = async (model, testingData) => {
-    const result = await model.evaluateDataset(testingData);
-    const testLoss = result[0].dataSync()[0];
-    const testAcc = result[1].dataSync()[0];
-
-    console.log(`Testing loss: ${testLoss.toFixed(4)}`);
-    console.log(`Testing Accuracy: ${testAcc.toFixed(4)}`);
-}
-
-const saveModel = async () => {
-    if (modelReady) {
-        try {
-            await model.save(`file://${path.join(__dirname, "../../client/public/model")}`);
-            return true;
-        } catch(exp) {
-            return false;
-        }
-    } else {
-        return false;
+const loadModel = async () => {
+    try {
+        model = await tf.loadLayersModel(`file://${path.join(__dirname, "../../client/public/model/model.json")}`);
+        modelReadyForPrediction = true;
+        return true;
+    } catch(exp) {
+        return exp;
     }
-}
+};
+
+const getDataSetPath = (dataSetName) => {
+    const fileNameFragment = getDataSetFilenameFragment(dataSetName);
+    const trainDataSetPath = `file://${path.resolve(path.join(__dirname, `../data/${fileNameFragment}_train.csv`))}`;
+    const testDataSetPath = `file://${path.resolve(path.join(__dirname, `../data/${fileNameFragment}_train.csv`))}`;
+
+    return [trainDataSetPath, testDataSetPath];
+};
 
 const loadData = (dataUrl) => {
     // normalize data values between 0-1
@@ -207,8 +175,50 @@ const loadData = (dataUrl) => {
         .batch(batchSize);
 };
 
+const trainModel = async (trainingData, epochs = epochsValue) => {
+    const options = {
+        epochs,
+        batchSize,
+        verbose: 0,
+        callbacks: {
+            onEpochBegin: (epoch) => {
+                console.log(`Epoch ${epoch + 1} of ${epochs}`);
+            },
+            onEpochEnd: (epoch, logs) => {
+                console.log(`Training set loss: ${logs.loss.toFixed(4)}`);
+                console.log(`Training set accuracy: ${logs.acc.toFixed(4)}`);
+            }
+        }
+    };
+
+    return await model.fitDataset(trainingData, options);
+};
+
+const evaluateModel = async (testingData) => {
+    const result = await model.evaluateDataset(testingData);
+    const testLoss = result[0].dataSync()[0];
+    const testAcc = result[1].dataSync()[0];
+
+    console.log(`Testing loss: ${testLoss.toFixed(4)}`);
+    console.log(`Testing Accuracy: ${testAcc.toFixed(4)}`);
+    modelReadyForPrediction = true;
+};
+
+const saveModel = async () => {
+    if (modelReadyForPrediction) {
+        try {
+            await model.save(`file://${path.join(__dirname, "../../client/public/model")}`);
+            return true;
+        } catch(exp) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+};
+
 const predict = async (imageUrl) => {
-    if (modelReady) {
+    if (modelReadyForPrediction) {
         const pixelData = await toPixelData(imageUrl, 255);
         const imageTensor = tf.tensor(pixelData, [imageWidth, imageHeight, imageChannels]);
         const inputTensor = imageTensor.expandDims();
@@ -230,23 +240,27 @@ const predict = async (imageUrl) => {
     } else {
         return null;
     }
-}
+};
 
-const createAndTrainModel = async () => {
-    const trainingData = loadData(`file://${path.resolve(path.join(__dirname, "../data/a_z_dataset_train2.csv"))}`);
-    const testingData = loadData(`file://${path.resolve(path.join(__dirname, "../data/a_z_dataset_test2.csv"))}`);
+const loadDataAndTrainModel = async (dataSetName) => {
+    const [ trainDataSetPath, testDataSetPath ] = getDataSetPath(dataSetName);
+    const trainingData = loadData(trainDataSetPath);
+    const testingData = loadData(testDataSetPath);
 
-    model = createModel();
-    const info = await trainModel(model, trainingData);
-    console.log(info);
+    if (modelCreated || modelReadyForPrediction) {
+        const info = await trainModel(trainingData);
+        console.log(info);
 
-    await evaluateModel(model, testingData);
-    modelReady = true;
-    saveModel();
+        await evaluateModel(testingData);
+        saveModel();
+    } else {
+        console.log("Model not available! Load or create model first to initiate (re)training.");
+    }
 };
 
 module.exports = {
-    createAndTrainModel,
-    predict,
+    createModel,
     loadModel,
+    loadDataAndTrainModel,
+    predict,
 };
